909432a91e45100345fbffca6a9bd6f62402acec
thaivm-0609 <vuongthai95@gmail.com>
2024-07-11 14:06:07 +0700

    feat: connect mysql and list

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 const express = require('express'); //require module express
 //từ express 4.16, ko cần require body-parser
 // var bodyParser = require('body-parser'); //require body-parser
-var multer = require('multer'); //require multer
+var multer = require('multer'); //multer để upload file ảnh
+const mysql = require('mysql'); //kết nối với CSDL
+var fs = require('fs'); //để xóa file ảnh
 const app = new express();
 const port = 3000; //khai báo cổng sẽ chạy server
 
@@ -10,8 +12,16 @@ const port = 3000; //khai báo cổng sẽ chạy server
 app.set('view engine', 'ejs');
 app.set('views', './views');
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public')); 
 app.use(express.urlencoded({ extended: true })); //từ express 4.16 trở đi
+
+//tạo kết nối với CSDL
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wd18411'
+})
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -28,27 +38,12 @@ app.get('/list', (req,res,next) => {
     //req.params: nằm trong url
     //req.query: ngăn cách bằng dấu ?query1=x&query2=y
     //req.body
-    let products = [
-        {
-            id: 1,
-            name: 'thaivm2',
-            price: 100000,
-            images: 'https://picsum.photos/100'
-        },
-        {
-            id: 2,
-            name: 'thaivm22',
-            price: 300000,
-            images: 'https://picsum.photos/100'
-        },
-        {
-            id: 3,
-            name: 'thaivm23',
-            price: 200000,
-            images: 'https://picsum.photos/100'
-        }
-    ];
-    res.render('list', { pros: products });
+    let sql = 'SELECT * FROM products'; //khai báo câu truy vấn
+    db.query(sql, (err,data) => { //thực thi câu truy vấn
+        if (err) throw err
+
+        res.render('list', { pros: data }); //trả data về giao diện list.ejs
+    })
 });
 
 app.get('/create', (req,res,next) => {
@@ -57,8 +52,20 @@ app.get('/create', (req,res,next) => {
 
 //router upload file ảnh
 app.post('/upload', upload.single('img'), (req,res) => {
-
 })
+
+//xóa file đã upload
+app.get('/delete', (req,res) => {
+    var img = req.query.img; //truyền tên ảnh cần xóa qua query
+    
+    if (img) { //nếu có tên ảnh trong query thì xóa
+        //fs.unlink('đường dẫn đến file cần xóa')
+        fs.unlink(`public/images/${img}`, (err, data) => {
+            if (err) throw err
+            console.log('file deleted');
+        })
+    }
+});
 
 app.post('/save', (req,res) => {
     //lấy giá trị a,b,c người dùng nhập qua form
